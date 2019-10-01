e73b840f4935bdde6b167e2820db686e2bb01327
Will Sutter <kingsutter12@gmail.com>
2019-10-01 15:42:37 -0500

    set up conditionals in absratced bonus calculation

--- client.js
+++ client.js
@@ -39,5 +39,39 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+x = {
+  name: 'Mayella',
+  employeeNumber: '89068',
+  annualSalary: '35000',
+  reviewRating: 1
+};
+function calculateBonus(rating, number, income){
+  let bonus = 0;
+  if (rating == 3) {
+    bonus += .04;
+  }
+  else if (rating == 4){
+    bonus += .06;
+  }
+  else if (rating == 5){
+    bonus += .1;
+  }
+  if (number.length == 4 && bonus <= .08){
+    bonus += .05;
+  }
+  if (income > 65000 && bonus > 0){
+    bonus -= .01;
+  }
+  return bonus * income;
+}
+function newEmployee(employee) {
+  
+  employeeBonus = {
+    name: employee.name,
+    bonusPercentage: null,
+    totalCompensation: null,
+    totalBonus: null,
+  }
+}
 
-console.log( employees );
+console.log(newEmployee(x));
