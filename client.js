6e1dd0bdccc9769954dac5c818976e7c16d24cb5
Will Sutter <kingsutter12@gmail.com>
2019-10-01 16:42:42 -0500

    add comments and cleaned up code a bit

--- client.js
+++ client.js
@@ -31,26 +31,12 @@ const employees = [
   }
 ];
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
-
-// Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
-// This problem is massive! Break the problem down. Use the debugger.
-// What is the fewest lines of code I can write and test to get just a little closer?
-
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
-x = {
-  name: 'Mayella',
-  employeeNumber: '89068',
-  annualSalary: '35000',
-  reviewRating: 1
-};
 function calculateBonus(employee){
   // this variables should keep the rest of the function code clean
   let rating = employee.reviewRating;
   let number = employee.employeeNumber;
   let income = employee.annualSalary;
-  let bonus = 0;
+  let bonus = 0; // this will be adjusted through the statements below
   if (rating == 3) {
     bonus += .04; // if rating is 3, increase bonus by 4%
   }
@@ -60,24 +46,31 @@ function calculateBonus(employee){
   else if (rating == 5){
     bonus += .1; // if rating is 5, increase bonus by 10%
   }
-  if (number.length == 4 && bonus <= .08){
-    bonus += .05; // if employee number is 4 digits long and bonus won't go to 13%, increase by 5%
+  if (number.length == 4){
+    bonus += .05; // if employee number is 4 digits long, increase by 5%
   }
   if (income > 65000 && bonus >= .01){
-    bonus -= .01; // if income is greater than $65,000, reduce bonus by 1% if not already at 0.
+    bonus -= .01; // if income is greater than $65,000, reduce bonus by 1% if not already greater than 1%.
+  }
+  if (bonus > .13){ //prevents bonus from going over 13%
+    bonus = .13;
   }
   return bonus;
 }
-function newEmployee(employee) {
+function bonusInfo(employee) {
   baseBonus = calculateBonus(employee);
-  income = Number(employee.annualSalary);
+  income = Number(employee.annualSalary); // type coercion to Number to prevent string concatenation
   employeeBonus = {
-    name: employee.name,
-    bonusPercentage: baseBonus,
-    totalCompensation: baseBonus*income + income,
-    totalBonus: Math.round(baseBonus*income)
+    name: employee.name, // the employee's name
+    bonusPercentage: baseBonus, // base bonus percentage
+    totalCompensation: baseBonus*income + income, // total bonus
+    totalBonus: Math.round(baseBonus*income) // generate bonus with income, rounded
   }
   return employeeBonus;
 }
 
-console.log(newEmployee(employees[1]));
+// Loop over all employees to calculate bonus
+
+for (let index = 0; index < employees.length; index++) {
+  console.log(bonusInfo(employees[index]));
+}
